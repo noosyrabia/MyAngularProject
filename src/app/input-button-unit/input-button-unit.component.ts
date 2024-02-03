fbb99ea4f9bb8c6bcd16f5dc96a14e8e03337f31
Nosy <stephanie.noos@gmail.com>
2024-02-03 14:00:41 +0100

    création d'un class

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './input-button-unit.component.scss'
 })
 export class InputButtonUnitComponent {
-  title ='Learn about components';
+  title ='Hello World!'; 
+  // constructor() { 
+  //   this.title = 'I Love Angular';
+  // }
+
+  constructor() { 
+    console.log("constructeur")
+    this.changeTitle('My First Angular App')
+    ;
+  }
+
+  changeTitle(newTitle : string): void {
+    console.log("avant change title");
+    this.title = newTitle;
+    console.log("après change title")
+  
+}
+
 }
